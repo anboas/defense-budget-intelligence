10dc5a49ff558c84a3f4ba8d5542132e3502b964
anboas <anboas@users.noreply.github.com>
2026-09-18 19:42:36 +0000

    deploy: bc5a1183b3681fa67a0bd01e0c88194c940ebb59

--- assets/copy-0Zzx0OXm.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./index-Cv8eUY1o.js";var t=e(`bookmark`,[[`path`,{d:`m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z`,key:`1fy3hk`}]]),n=e(`copy`,[[`rect`,{width:`14`,height:`14`,x:`8`,y:`8`,rx:`2`,ry:`2`,key:`17jyea`}],[`path`,{d:`M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2`,key:`zix9uf`}]]);export{t as n,n as t};
